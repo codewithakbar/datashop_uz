0094e51e1358907ccd66420a2ea6bccbdbbcc845
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-26 00:00:51 +0500

    static root add

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -143,6 +144,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
