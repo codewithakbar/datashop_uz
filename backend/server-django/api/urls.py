b9c197038b44a71f63e2a083c1024d6fb63e699d
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-01-07 01:09:11 +0500

    django push

--- backend/server-django/api/urls.py
+++ backend/server-django/api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from .views import UserDetailAPI,RegisterUserAPIView
-from rest_framework_simplejwt.views import (
-        TokenObtainPairView,
-        TokenRefreshView,
-)
-from api.views import MyObtainTokenPairView, RegisterView
+from . import views
+from .views import MyTokenObtainPairView
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path("get-details",UserDetailAPI.as_view()),
-    path('register',RegisterUserAPIView.as_view()),
+    path('', views.getRoutes),
+    path('notes/', views.getNotes),
 
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
--- backend/server-django/api/views.py
+++ backend/server-django/api/views.py
@@ -1,54 +1,46 @@
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from projects.models import Project
 
-from .serializers import ProjectSerializer
-
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import UserSerializer,RegisterSerializer
-from django.contrib.auth.models import User
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import generics
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
+from .serializers import UserSerializer
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def getProjects(request):
 
-    projects = Project.objects.all()
-    serialize = ProjectSerializer(projects, many=True)
-    return Response(serialize.data)
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
 def getRoutes(request):
-
     routes = [
-        {'GET' : 'api/projects/'},
-        {'POST' : 'api/users/token'},
+        '/api/token',
+        '/api/token/refresh',
     ]
+
     return Response(routes)
 
 
-
-class UserDetailAPI(APIView):
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
-
-    def get(self,request,*args,**kwargs):
-        user = User.objects.get(id=request.user.id)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-
-#Class based view to register user
-class RegisterUserAPIView(generics.CreateAPIView):
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
-
-
-
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = UserSerializer(notes, many=True)
+    return Response(serializer.data)
