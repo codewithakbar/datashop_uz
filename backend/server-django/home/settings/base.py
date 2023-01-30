9b3847e4880fecf4ec106f69a4856242e01b1ee7
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-01-30 16:53:09 +0500

    add allowed host api.datashop.uz

--- backend/server-django/home/settings/base.py
+++ backend/server-django/home/settings/base.py
@@ -4,8 +4,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["api.datashop.uz", ".datashop.uz"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
