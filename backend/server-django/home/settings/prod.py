3c57a9c2a28f0f156f9a1593b2723696310444c6
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-01-30 17:10:18 +0500

    default database sqlite3

--- backend/server-django/home/settings/prod.py
+++ backend/server-django/home/settings/prod.py
@@ -3,17 +3,24 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS += ['http://domain.com']
+ALLOWED_HOSTS += ['.datashop.uz']
 WSGI_APPLICATION = 'home.wsgi.prod.application'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_name',
+#         'USER': 'db_user',
+#         'PASSWORD': 'db_password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_name',
-        'USER': 'db_user',
-        'PASSWORD': 'db_password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
